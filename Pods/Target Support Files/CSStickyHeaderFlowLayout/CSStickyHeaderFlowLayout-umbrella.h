40aec071cdbb217f4ae7fc86489104e901093f08
Roman Ustiantcev <liberrom.hse@gmail.com>
2017-05-26 22:45:49 +0200

    Pods added

---
+++ Pods/Target Support Files/CSStickyHeaderFlowLayout/CSStickyHeaderFlowLayout-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CSStickyHeaderFlowLayout.h"
+#import "CSStickyHeaderFlowLayoutAttributes.h"
+
+FOUNDATION_EXPORT double CSStickyHeaderFlowLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char CSStickyHeaderFlowLayoutVersionString[];
+
---
+++ Pods/Target Support Files/Pods-CSSickyFlowLayoutHeaders/Pods-CSSickyFlowLayoutHeaders-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSSickyFlowLayoutHeaders : NSObject
+@end
+@implementation PodsDummy_Pods_CSSickyFlowLayoutHeaders
+@end
