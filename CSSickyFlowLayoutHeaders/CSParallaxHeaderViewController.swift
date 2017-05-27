979e61f9c4c0850274e10b58be14a6c1359cf4b1
Roman Ustiantcev <liberrom.hse@gmail.com>
2017-05-27 12:18:39 +0200

    CSParallaxHeader - added

--- CSSickyFlowLayoutHeaders/CSParallaxHeaderViewController.swift
+++ CSSickyFlowLayoutHeaders/CSParallaxHeaderViewController.swift
@@ -12,14 +12,27 @@ import CSStickyHeaderFlowLayout
 private let reuseIdentifier = "Cell"
 
 class CSParallaxHeaderViewController: UICollectionViewController {
+    
+    let sections: Array<Dictionary<String, String>> = [["Twitter":"http://twitter.com",
+                                                "Facebook":"http://facebook.com",
+                                                "Tumblr":"http://tumblr.com",
+                                                "Pinterest":"http://pinterest.com",
+                                                "Instagram":"http://instagram.com",
+                                                "Github":"http://github.com"]]
+    
+    private var layout : CSStickyHeaderFlowLayout? {
+        return self.collectionView?.collectionViewLayout as? CSStickyHeaderFlowLayout
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
         // Register cell classes
+        let headerNib = UINib(nibName: "CSParallaxHeader", bundle: nil)
+        self.collectionView?.register(headerNib, forSupplementaryViewOfKind: CSStickyHeaderParallaxHeader, withReuseIdentifier: "header")
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
@@ -30,66 +43,44 @@ class CSParallaxHeaderViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    @IBAction func reloadButtonDidPress(sender: AnyObject) {
+        let indexSet = NSIndexSet(indexesIn: NSMakeRange(0, (self.collectionView!.numberOfSections)))
+        self.collectionView?.reloadSections(indexSet as IndexSet)
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return self.sections.count
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CSCell
+        let obj = self.sections[indexPath.section]
+        
+        cell.textLabel.text = obj.values.first
+        
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == CSStickyHeaderParallaxHeader {
+            let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) 
+            return cell
+        } else {
+            let obj = self.sections[indexPath.section]
+            let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "sectionHeader", for: indexPath) as! CSSectionHeader
+            cell.textLabel.text = obj.keys.first
+            return cell
+        }
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
+
